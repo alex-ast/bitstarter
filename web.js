17d9090aab0732aef9b7bc3fee2ab1c1d6b93fa0
Alex Astapchuk <alex.astapchuk@gmail.com>
2013-12-08 18:20:21 +0000

    web.js to send index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
+var IndexFile='index.html';
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ //response.send('Hello World 2!');
+ var fileText = fs.readFileSync(IndexFile, 'ascii');
+ response.send(fileText); 
 });
 
 var port = process.env.PORT || 5000;
